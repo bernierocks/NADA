6aafbad9ca4ce709f29d36f2cbc4cc91151d6e71
Bernie Colita <kittywebworks@gmail.com>
2015-09-01 03:45:54 +0800

    cc

--- footer.php
+++ footer.php
@@ -19,7 +19,7 @@
 
 <script>
 $("document").ready(function() {
-    $(".tagline p").fitText(1.2, { minFontSize: '3px', maxFontSize: '12px' });
+    $(".tagline p").fitText(1, { minFontSize: '3px', maxFontSize: '12px' });
 });
    
 </script>
