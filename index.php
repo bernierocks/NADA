7fbd8f0c71bef8a2d40d88a4c0e8bc64abfc2772
Bernie Colita <kittywebworks@gmail.com>
2015-09-01 04:22:09 +0800

    cc

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ include('header.php'); // Include pulls another file into the page, and sticks i
         
 
         </div><!-- /.col8-->
-        <div class="col col-sm-12 col-md-4"> 
+        <div class="col col-sm-12 col-md-4 sidebar"> 
             Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. 
             
             <div class="sponsors">
