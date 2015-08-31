98a774c59daa8be8760be0775d52f44258238896
Bernie Colita <kittywebworks@gmail.com>
2015-09-01 04:10:19 +0800

    cc

--- index.php
+++ index.php
@@ -3,4 +3,19 @@ $home = 'Y'; // This variable designates this as the index page, and tells the h
 include('header.php'); // Include pulls another file into the page, and sticks it on where the include is called.
 ?>
 
+<div class="container">   
+    <div class="row">  
+        <div class="col col-sm-12 col-md-8">  
+        
+
+        </div><!-- /.col8-->
+        <div class="col col-sm-12 col-md-4"> 
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. 
+        </div><!-- /.col4-->
+    </div><!-- /.row -->
+</div><!-- /.container -->
+
+
 <?include('footer.php'); ?>
+
+
