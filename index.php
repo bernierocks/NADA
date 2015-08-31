d7d10db21204d53297f4cf05f283158504c40171
Bernie Colita <kittywebworks@gmail.com>
2015-09-01 04:16:04 +0800

    cc

--- index.php
+++ index.php
@@ -11,6 +11,13 @@ include('header.php'); // Include pulls another file into the page, and sticks i
         </div><!-- /.col8-->
         <div class="col col-sm-12 col-md-4"> 
             Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. 
+            
+            <div class="sponsors">
+                <a href="http://www.nadsa.org/" title="Member of NADSA">
+                    <img src="images/logo_ncadsa.jpg">
+                </a>
+                
+            </div><!-- /.sponsors -->
         </div><!-- /.col4-->
     </div><!-- /.row -->
 </div><!-- /.container -->
