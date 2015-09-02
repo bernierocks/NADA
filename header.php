ddab497ff3201ab38570b3d43fe2ea2cfa761714
Bernie Colita <kittywebworks@gmail.com>
2015-09-02 22:56:28 +0800

    cc

--- header.php
+++ header.php
@@ -63,6 +63,11 @@ if($home!='Y' && $_GET['mms'] != 'X'){ //this IF statement is the test.  If thes
               <div class="col col-xs-2 col-sm-2 col-md-2 logo">   
                 <img alt="" src="images/logo.png">
                </div><!-- /.col2-->
+               <div class="col col-xs-8 col-sm-8 col-md-8">  
+                   <h1>
+                       North Carolina Adult Day Services Association
+                   </h1>
+                </div><!-- /.col8-->
                </div><!-- /.row -->
            </div><!-- /.header -->
            
