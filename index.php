2fd808050f3f6256d51190432fb3cd8957e7eee6
Bernie Colita <kittywebworks@gmail.com>
2015-09-04 02:42:15 +0800

    cc

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 $home = 'Y'; // This variable designates this as the index page, and tells the header to not put the title on this home page.
 include('header.php'); // Include pulls another file into the page, and sticks it on where the include is called.
 ?>
-
+<div class="container">   
     <div class="row">  
 
         <div class="col col-sm-12 col-md-4 col-md-push-8 sidebar"> 
@@ -31,7 +31,7 @@ include('header.php'); // Include pulls another file into the page, and sticks i
         </div><!-- /.col8-->
 
     </div><!-- /.row -->
-
+</div><!-- /.container -->
 
 <?include('footer.php'); ?>
 
